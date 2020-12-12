3a60c0663fbd5d51423f74f9732ba13277824261
Metanix777 <shukalo.96@gmail.com>
2020-12-12 20:07:39 +0300

    feat: added map

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,4 @@
 import '../css/style.css';
+import setupMap from './map-js/map';
+
+setupMap();
---
+++ src/js/map-js/map.js
@@ -0,0 +1,12 @@
+export default function setupMap() {
+    const mapId = document.querySelector('#map');
+    const mapOptions = {
+        center: [32.526955973553164, 23.047893209259385],
+        zoom: 2
+    }
+
+    const map = new L.map(mapId, mapOptions);
+    const layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+
+    map.addLayer(layer);
+}

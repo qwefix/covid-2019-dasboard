b9682d50825ad9b26345d26a479d7d006a189f99
kleo4004 <zkioz4004@gmail.com>
2020-12-12 19:03:07 +0300

    feat: add list section and slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,7 @@
 import '../css/style.css';
+import Slider from './views/slider';
+
+window.addEventListener('DOMContentLoaded', () => {
+    const slider = new Slider('.slides-wrapper', '.slide', '.arrow-left', '.arrow-right', '.current', '.total');
+    slider.render();
+});
---
+++ src/js/views/slider.js
@@ -0,0 +1,44 @@
+export default class Slider {
+    constructor(page, slide, arrowLeft, arrowRight, current, total) {
+        this.page = document.querySelector(page);
+        this.slides = document.querySelectorAll(slide);
+        this.arrowLeft = document.querySelector(arrowLeft);
+        this.arrowRight = document.querySelector(arrowRight);
+        this.current = document.querySelector(current);
+        this.total = document.querySelector(total);
+        this.slideIndex = 1;
+        this.totalNum =  this.slides.length;
+        this.currentNum = this.slideIndex;
+    }
+
+    showSlides(n) {
+        this.total.textContent = this.totalNum;
+        this.currentNum = this.slideIndex;
+        if (n > this.slides.length) {
+            this.slideIndex = 1;
+            this.currentNum = 1;
+        } 
+        if (n < 1) {
+            this.slideIndex = this.slides.length;
+            this.currentNum = this.slides.length;
+        }
+        this.current.textContent = this.currentNum;
+        this.slides.forEach(slide => slide.style.display = 'none');
+        this.slides[this.slideIndex - 1].style.display = 'block';
+    }
+
+    presSlide(n) {
+        this.showSlides(this.slideIndex += n);
+    }
+
+    render() {
+        this.arrowLeft.addEventListener('click', () => {
+            this.presSlide(-1)
+        });
+        this.arrowRight.addEventListener('click', () => {
+            this.presSlide(1)
+        })
+        this.showSlides(this.slideIndex);
+    }
+
+}

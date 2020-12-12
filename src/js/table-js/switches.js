12b3f0d4838c1cd85f8a1a6f0787b4229b23dc4a
Hramchenlo Alexey <qwefix@gmail.com>
2020-12-12 19:32:59 +0300

    feat: last table

--- src/js/table-js/switches.js
+++ src/js/table-js/switches.js
@@ -1,10 +1,9 @@
 function switcherForTable(evt){
     const switcher = evt.target.closest('div.switch');
-    if (switcher === null) return
-    switcher.querySelectorAll('p').forEach(v=>v.classList.toggle('on'))
-
+    if (switcher === null) return;
+    switcher.querySelectorAll('*').forEach(v=>v.classList.toggle('on'));
 }
 function addTableSwitchListeners(){
-    document.querySelectorAll('#table .settings').forEach(a=>a.addEventListener('click',switcherForTable,{capture:true}))
+    document.querySelectorAll('#table .settings').forEach(a=>a.addEventListener('click',switcherForTable))
 }
 export default addTableSwitchListeners;

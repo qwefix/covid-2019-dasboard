1b016f82da90e270717986626a51c65679660872
Metanix777 <shukalo.96@gmail.com>
2020-12-13 21:18:25 +0300

    added marker

--- src/js/map-js/map.js
+++ src/js/map-js/map.js
@@ -9,4 +9,21 @@ export default function setupMap() {
     const layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 
     map.addLayer(layer);
+
+    const data = [
+        {coordinates: [17.385044, 78.486671]},
+        {coordinates: [53.881297252241644, 27.569905821286902]},
+        {coordinates: [16.506174, 80.648015]}
+    ];
+    data.forEach(element => {
+        const circleRadius = 200000/100;
+        const circleOption = {
+            color: 'red',
+            fillOpacity: 100
+        }
+
+        const circle = new L.circle(element.coordinates, circleRadius, circleOption);
+        circle.addTo(map);
+    });
+    
 }

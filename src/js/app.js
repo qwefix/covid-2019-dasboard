bf8618d79f900c8e959e477161a0795bf669d13b
Alexey <qwefix@gmail.com>
2020-12-15 17:38:22 +0300

    refactor:rename chunk

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import '../css/style.css';
 import Slider from './views/slider';
-import table from './table-js/set-table-data';
+import table from './table-js/table';
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new Slider('.slides-wrapper', '.slide', '.arrow-left', '.arrow-right', '.current', '.total');

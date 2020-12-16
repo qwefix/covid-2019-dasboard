18babbc7d0ad476f5fe4917bf40ad8a7e3044e5a
Hramchenlo Alexey <qwefix@gmail.com>
2020-12-16 12:46:48 +0300

    feat: graph

--- src/js/apiUrlConfig/apiUrl.js
+++ src/js/apiUrlConfig/apiUrl.js
@@ -1,6 +1,8 @@
 const urls = {
     covid19api: 'https://api.covid19api.com',
-    populationInfoApi: 'https://restcountries.eu/rest/v2/all?fields=population;alpha2Code'
+    populationInfoApi: 'https://restcountries.eu/rest/v2/all?fields=population;alpha2Code',
+    graphGlobal: 'https://disease.sh/v3/covid-19/historical/all?lastdays=366',
+    
 };
 
 export default urls;
--- src/js/graph-js/graph.js
+++ src/js/graph-js/graph.js
@@ -1,18 +1,19 @@
 import Chart from 'chart.js';
+import urls from '../apiUrlConfig/apiUrl';
 
 class Graph {
     constructor(){
         this.canvas = document.getElementById('graphCanvas');
-        this.config = {
+        this.input = {
             type: 'bar',
             data: {
                 labels: ['1', '2', '3', '4', '5', '6'],
                 datasets: [
                     {
-                        barPercentage: 0.4,
                         label: 'deaths',
                         data: [12, 19, 3, 5, 2, 3],
                         backgroundColor: 'red',
+                        barPercentage: 1.3,
                     },
                 ]
             },
@@ -29,24 +30,49 @@ class Graph {
                     }],
                     xAxes: [{
                         ticks: {
-                            // display: false
+                            display: false
                         }
                     }]
                 }
             }
         };
-        this.chartJS = new Chart(this.canvas, this.config);
+        this.chartJS = new Chart(this.canvas, this.input);
         this.setup();
     }
 
     setup(){
-        this.promice = fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=366")
+        this.promice = fetch(urls.graphGlobal)
             .then(res => {
                 if (res.status !== 200) {
                     return Promise.reject(res);
                 }
                 return res.json();
-            })  
+            })
+            .then((res) => {
+                this.worldData ={ 
+                    lables: Object.entries(res.cases).map(a=>a[0]),
+                    cases: Object.entries(res.cases).map(a=>a[1]),
+                    deaths: Object.entries(res.deaths).map(a=>a[1]),
+                    recovered: Object.entries(res.recovered).map(a=>a[1]),
+                }
+                this.renderGlobal();
+                console.log(this.worldData)
+            })
+    }
+    renderGlobal(){
+        this.input.data.labels = this.worldData.lables;
+        let colors = ['grey','red','green'];
+        this.input.data.datasets=[];
+        ['cases','deaths','recovered'].forEach((a,i)=>{
+            this.input.data.datasets.push({
+                label: a,
+                data: this.worldData[a],
+                backgroundColor: colors[i],
+                barPercentage: 1.3,
+            })
+        })
+        this.chartJS.update();
+        console.log(this.input);
     }
 }
 

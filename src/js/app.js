aed9de26e2b5e7ebef54e2ce46fac89e04d7e4fa
kleo4004 <zkioz4004@gmail.com>
2020-12-16 20:41:48 +0300

    feat: add GlobalCases class

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,21 @@
 import '../css/style.css';
 import table from './table-js/table';
 import list from './list-js/list';
+import globalCases from './globalCases-js/globalCases';
 import selectCountry from './selectCountry';
 
 window.addEventListener('DOMContentLoaded', () => {
     const searchInput = document.getElementById('search');
     const searchForm = document.getElementById('search-form');
    list.setupListData();
+   globalCases.setupGlobalCasesData();
     document.addEventListener('click', (event) => {
         if (event.target.getAttribute('class') === "target-country") {
             let searchText = event.target.textContent;
+            selectCountry(searchText);
+        }
+        if (event.target.getAttribute('id') === "global-cases") {
+            let searchText = 'Global';
             selectCountry(searchText);
         }
     });
---
+++ src/js/globalCases-js/globalCases.js
@@ -0,0 +1,27 @@
+import urls from '../apiUrlConfig/apiUrl';
+
+class GlobalCases {
+    constructor(apiUrls) {
+        this.covid2019Api  = apiUrls.covid19api;
+        this.globalCasesBlock = document.getElementById('global-cases');
+    }
+    setupGlobalCasesData() {
+        fetch(`${this.covid2019Api}/summary`)
+            .then(res => {
+                if (res.status !== 200) {
+                    return Promise.reject(res);
+                }
+                return res.json();
+            })
+            .then((res) => {
+               this.globalInfo = res.Global.TotalConfirmed;
+               this.getGlobalInfo(this.globalInfo);
+            })     
+    };
+    getGlobalInfo(casesNumber) {
+        this.globalCasesBlock.innerHTML = `Cases in world: ${casesNumber}`;
+    }
+}
+
+let globalCases = new GlobalCases(urls);
+export default globalCases;

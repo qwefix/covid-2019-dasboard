69284bec18ec8c87830e525d346049209e12c4f3
Hramchenlo Alexey <qwefix@gmail.com>
2020-12-17 12:19:48 +0300

    refactor: start from world

--- src/js/app.js
+++ src/js/app.js
@@ -8,4 +8,3 @@ window.addEventListener('DOMContentLoaded', () => {
     const slider = new Slider('.slides-wrapper', '.slide', '.arrow-left', '.arrow-right', '.current', '.total');
     slider.render();
 });
-graph.render('sOuth Africa')

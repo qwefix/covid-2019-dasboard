4345388a6404e036715ffc615ae3b1e952281de7
kleo4004 <zkioz4004@gmail.com>
2020-12-18 15:58:46 +0300

    feat: add autocomplete to search

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,19 @@
 import '../css/style.css';
-import table from './table-js/table';
-import graph from './graph-js/graph';
+// import table from './table-js/table';
+// import graph from './graph-js/graph';
 import list from './list-js/list';
 import globalCases from './globalCases-js/globalCases';
 import selectCountry from './selectCountry';
+import autocompleteCountries from './views/autocomplete';
 
 window.addEventListener('DOMContentLoaded', () => {
-    const searchInput = document.getElementById('search');
+    const searchInput = document.getElementById('search-input');
     const searchForm = document.getElementById('search-form');
-   list.setupListData();
-   globalCases.setupGlobalCasesData();
+
+    list.setupListData();
+    globalCases.setupGlobalCasesData();
+    autocompleteCountries();
+
     document.addEventListener('click', (event) => {
         if (event.target.getAttribute('class') === "target-country") {
             let searchText = event.target.textContent;
@@ -19,11 +23,16 @@ window.addEventListener('DOMContentLoaded', () => {
             let searchText = 'Global';
             selectCountry(searchText);
         }
+        if (event.target.getAttribute("class") === "autocomplete-items") {
+            closeAllLists(event.target);
+        }
     });
     searchForm.addEventListener('submit', (event) => {
         event.preventDefault();
         let searchText = searchInput.value;
         selectCountry(searchText);
+        searchForm.reset();
     });
+   
 });
 
